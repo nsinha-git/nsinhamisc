d01bf23cee822a3b989c865e8318f7748b4a2096
Nishchay Sinha <nsinha@Nishchays-MacBook-Pro.local>
2017-02-28 12:21:14 -0800

    name changes to test files

--- stocks/src/main/scala/com.nsinha/apps/OrdersApps/OrderAnalysis.scala
+++ stocks/src/main/scala/com.nsinha/apps/OrdersApps/OrderAnalysis.scala
@@ -5,14 +5,14 @@ import com.nsinha.impls.Project.Orders.CsvOrderScottradeProjectImpl
 import com.nsinha.impls.Project.Quotes.Scottrade.CsvDailyQuotesScottradeProjectImpl
 import main.scala.com.nsinha.data.Csv.generated.GenCsvQuoteRowScottrade
 
-/** Created by nishchaysinha on 10/20/16.
+/** Created by nsinha on 10/20/16.
   */
 object OrderAnalysis {
-  def apply(quoteFile : String, orderFile : String, byDateFile : String = "/Users/nishchaysinha/stocksdatadir/currentPerformance/currentHoldsByDate.json", bySymbolFile : String = "/Users/nishchaysinha/stocksdatadir/currentPerformance/currentHoldsBySymbol.json",
-            modelFileForQuote : String = "/Users/nishchaysinha/nsinhamisc/stocks/src/test/resources/modelforcsv.txt") = {
+  def apply(quoteFile : String, orderFile : String, byDateFile : String = "/Users/nsinha/stocksdatadir/currentPerformance/currentHoldsByDate.json", bySymbolFile : String = "/Users/nsinha/stocksdatadir/currentPerformance/currentHoldsBySymbol.json",
+            modelFileForQuote : String = "/Users/nsinha/mygithubs/nsinhamisc/stocks/src/test/resources/modelforcsv.txt") = {
 
     val quoteImpl = new CsvDailyQuotesScottradeProjectImpl(modelFilePath = modelFileForQuote, quotesFilePathInput = quoteFile, classzz = GenCsvQuoteRowScottrade.getClass)
-    val orderImpl = new CsvOrderScottradeProjectImpl(modelFilePath = "/Users/nishchaysinha/nsinhamisc/stocks/src/test/resources/modelforcsv.txt", orderFilePathInput = orderFile, quoteImpl)
+    val orderImpl = new CsvOrderScottradeProjectImpl(modelFilePath = "/Users/nsinha/mygithubs/nsinhamisc/stocks/src/test/resources/modelforcsv.txt", orderFilePathInput = orderFile, quoteImpl)
 
     orderImpl.dumpPerformanceCurrentHolds(byDateFile)
     orderImpl.dumpPerformanceCurrentHoldsGroupedOnSymbol(bySymbolFile)
@@ -22,9 +22,9 @@ object OrderAnalysis {
   }
 
   def run() = {
-    val quoteFile = "/Users/nishchaysinha/Downloads/Securities_to_Watch2016.12.27.12.50.37.csv"
-    val orderFile = "/Users/nishchaysinha/Downloads/CompletedOrders2016.12.27.12.46.57.csv"
-    OrderAnalysis(quoteFile, orderFile, "/Users/nishchaysinha/Desktop/byDateHolds.json", "/users/nishchaysinha/Desktop/bySymbolHolds.json")
+    val quoteFile = "/Users/nsinha/Downloads/Securities_to_Watch2017.02.28.15.09.32.csv"
+    val orderFile = "/Users/nsinha/Downloads/CompletedOrders2017.02.28.14.39.49.csv"
+    OrderAnalysis(quoteFile, orderFile, "/Users/nsinha/Desktop/byDateHolds.json", "/users/nsinha/Desktop/bySymbolHolds.json")
   }
 
   def main(args : Array[String]) {
